6ca3787936a01fa97e90102a3286b961ad05afbd
cesarzb <cesarzb@protonmail.com>
2026-01-25 14:26:43 +0100

    Fixed terminal toggle in nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,5 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
-vim.keymap.set("n", "<leader>ya", "<cmd>%y+<cr>", { desc = "Copy whole file to clipboard" })
+
+local map = vim.keymap.set
+
+map("n", "<leader>ya", "<cmd>%y+<cr>", { desc = "Copy whole file to clipboard" })
+
+map("t", "<C-/>", "<C-\\><C-n><cmd>close<cr>", { desc = "Hide Terminal" })
