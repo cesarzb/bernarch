f71794e438dadede71bcc294cfb6ac822cce3d68
cesarzb <cesarzb@protonmail.com>
2026-02-13 15:55:22 +0100

    Added markdown to nvim, and mouse config

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -7,4 +7,13 @@ return {
 			end, { desc = "Emmet: Wrap with tag" })
 		end,
 	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+	},
 }
